086d3366bacae02869f06d374ee4f5fd8e94d3c4
bell0-0-dot <bella_serrano@unitec.edu>
2026-09-12 08:57:27 -0600

    .

--- src/pokemon_battle/NodoPokemon.java
+++ src/pokemon_battle/NodoPokemon.java
@@ -9,5 +9,30 @@ package pokemon_battle;
  * @author vasqu
  */
 public class NodoPokemon {
+    private Pokemon pokemon;
+    private NodoPokemon siguiente;
+
+    public NodoPokemon(Pokemon pokemon, NodoPokemon siguiente) {
+        this.pokemon = pokemon;
+        this.siguiente = siguiente;
+    }
+
+    public Pokemon getPokemon() {
+        return pokemon;
+    }
+
+    public void setPokemon(Pokemon pokemon) {
+        this.pokemon = pokemon;
+    }
+
+    public NodoPokemon getSiguiente() {
+        return siguiente;
+    }
+
+    public void setSiguiente(NodoPokemon siguiente) {
+        this.siguiente = siguiente;
+    }
+    
+    
     
 }
