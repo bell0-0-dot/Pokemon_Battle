1d3cf3a8e099abdce83b9691ba66d18f2df2d1c4
bell0-0-dot <bella_serrano@unitec.edu>
2026-09-12 10:42:50 -0600

    imagenes

--- src/pokemon_battle/datos/Pokedex.java
+++ src/pokemon_battle/datos/Pokedex.java
@@ -13,26 +13,21 @@ public class Pokedex {
     }
 
     private void cargar() {
-        plantillas.insertar(new Pokemon("Bulbasaur", 14, null, 90, 26, 30));
-        plantillas.insertar(new Pokemon("Ivysaur", 17, null, 105, 32, 34));
-        plantillas.insertar(new Pokemon("Venusaur", 21, null, 130, 42, 40));
-        plantillas.insertar(new Pokemon("Charmander", 14, null, 85, 30, 24));
-        plantillas.insertar(new Pokemon("Charmeleon", 17, null, 105, 36, 30));
-        plantillas.insertar(new Pokemon("Charizard", 21, null, 128, 46, 36));
-        plantillas.insertar(new Pokemon("Squirtle", 14, null, 92, 26, 34));
-        plantillas.insertar(new Pokemon("Wartortle", 17, null, 108, 32, 40));
-        plantillas.insertar(new Pokemon("Blastoise", 21, null, 132, 40, 48));
-        plantillas.insertar(new Pokemon("Pikachu", 15, null, 95, 34, 24));
-        plantillas.insertar(new Pokemon("Raichu", 19, null, 115, 44, 30));
-        plantillas.insertar(new Pokemon("Jigglypuff", 13, null, 110, 20, 18));
-        plantillas.insertar(new Pokemon("Meowth", 14, null, 85, 28, 22));
-        plantillas.insertar(new Pokemon("Psyduck", 14, null, 90, 26, 26));
-        plantillas.insertar(new Pokemon("Machop", 15, null, 100, 35, 30));
-        plantillas.insertar(new Pokemon("Geodude", 15, null, 95, 30, 42));
-        plantillas.insertar(new Pokemon("Gastly", 14, null, 80, 32, 20));
-        plantillas.insertar(new Pokemon("Onix", 16, null, 110, 30, 46));
-        plantillas.insertar(new Pokemon("Eevee", 15, null, 95, 30, 28));
-        plantillas.insertar(new Pokemon("Snorlax", 22, null, 160, 40, 38));
+        plantillas.insertar(new Pokemon("/Imagenes/staryu.png","Staryu", 15, null, 95, 30, 28));
+        plantillas.insertar(new Pokemon("/Imagenes/Starmie.png","Starmie", 18, null, 115, 40, 32));
+        plantillas.insertar(new Pokemon("/Imagenes/Pysduck.png","Psyduck", 13, null, 85, 25, 26));
+
+        plantillas.insertar(new Pokemon("/Imagenes/Bulbasaur.jpg","Bulbasaur", 14, null, 90, 26, 30));
+        plantillas.insertar(new Pokemon("/Imagenes/Vileplume.png","Vileplume", 19, null, 120, 38, 34));
+        plantillas.insertar(new Pokemon("/Imagenes/Tangela.png","Tangela", 16, null, 105, 32, 36));
+
+        plantillas.insertar(new Pokemon("/Imagenes/Abra.png","Abra", 13, null, 75, 22, 20));
+        plantillas.insertar(new Pokemon("/Imagenes/Kadabra.png","Kadabra", 17, null, 100, 42, 24));
+        plantillas.insertar(new Pokemon("/Imagenes/Alakazam.png","Alakazam", 20, null, 115, 50, 26));
+
+        plantillas.insertar(new Pokemon("/Imagenes/Ekans.png","Ekans", 14, null, 88, 27, 25));
+        plantillas.insertar(new Pokemon("/Imagenes/Arbok.png","Arbok", 18, null, 112, 38, 32));
+        plantillas.insertar(new Pokemon("/Imagenes/Weezing.png","Weezing", 19, null, 118, 36, 40));
     }
 
     public int contar() {
@@ -71,7 +66,7 @@ public class Pokedex {
         if (plantilla == null) {
             return null;
         }
-        return new Pokemon(plantilla.getNombre(), plantilla.getNivel(), plantilla.getTipo(),
+        return new Pokemon(plantilla.getRutaImagen(),plantilla.getNombre(), plantilla.getNivel(), plantilla.getTipo(),
                 plantilla.getHp(), plantilla.getAtaque(), plantilla.getDefensa());
     }
 }
